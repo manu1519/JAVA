45b5717b44f82a2bcb4fea2f8750224cb4fff4e7
Manuel Mendoza Meza <42361573+manu1519@users.noreply.github.com>
2023-08-23 15:12:27 -0600

    Delete Tareas directory

--- Tareas/src/main/java/VW/Tareas/repo/Tarearepo.java
+++
@@ -1,7 +0,0 @@
-package VW.Tareas.repo;
-
-import VW.Tareas.modelo.tarea;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Tarearepo extends JpaRepository<tarea, Integer> {
-}
